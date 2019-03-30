af65b5bda3268f44663a7be50f5634a14ea98a1a
Bubun Das <bubundas17@gmail.com>
2019-03-29 20:11:48 -0700

    Alpha Build

---
+++ library.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var plugin = {};
+
+plugin.parse = function(data, callback) {
+	if (!data || !data.postData || !data.postData.content) {
+	    return callback(null, data);
+	}
+
+	plugin.parseRaw(data.postData.content, function (err, content) {
+		data.postData.content = content;
+		callback(err, data);
+	});
+};
+
+plugin.parseRaw = function (content, callback) {
+	callback(null, 
+		content.replace(/\[hide\](.*?)\[\/hide\]/s, 'Hidden Content')
+	);
+};
+
+module.exports = plugin;
